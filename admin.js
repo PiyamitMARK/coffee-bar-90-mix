794349c9e4caf9441387586678040213b5a6f4bd
PiyamitMARK <zzmarkzz1329@gmail.com>
2026-04-01 12:04:03 +0700

    Update admin.js

--- admin.js
+++ admin.js
@@ -11,7 +11,7 @@ const ORDERS_STORAGE_KEY = 'tea-coffee-pos-orders';
 const ORDER_NUMBER_KEY = 'tea-coffee-pos-orderNumber';
 
 // ==================== Google Sheet Config ====================
-const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycby0zo42oZdNXnpjjERmKgl3aPcFKcViT7qPV7BqO5Hwqpk0nyRBUP8zJnDeE7ylvy-bVQ/exec';
+const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbw5u8ATWKZsdFIrpGeTxdA6z7VpFkKeDE3uf_WcWido7YSAMQYGr9uwO5_rEnFhU41BBg/exec';
 // =============================================================
 
 const loginScreen = document.getElementById('loginScreen');
