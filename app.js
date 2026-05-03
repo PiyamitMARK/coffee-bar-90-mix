bc69e739cd56a1753e2934a36b1a342947b168f1
PiyamitMARK <zzmarkzz1329@gmail.com>
2026-05-03 13:11:53 +0700

    Update app.js

--- app.js
+++ app.js
@@ -51,7 +51,8 @@ const products = {
   ],
   kaosoi: [
     { id: 'soi1', name: 'ข้าวซอยไก่', price: 65, image: IMG(111) },
-    { id: 'soi2', name: 'กะเพรา', price: 50, image: IMG(111) },
+    { id: 'soi2', name: 'กะเพรา', price: 50, image: IMG(10000) },
+    { id: 'soi2', name: 'ลาบหมู', price: 60, image: IMG(10001) },
     { id: 'soi3', name: 'น้ำเงี้ยว', price: 60, image: IMG(555) },
     { id: 'soi5', name: 'เพิ่มไก่', price: 20, image: IMG(798798) },
     { id: 'soi4', name: 'แคบหมู', price: 15, image: IMG(98789) },
